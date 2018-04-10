6aaf0d6386e3c95a3347a5f25e4031f7576bfd30
Mark Brooks <Markabrooks@gmail.com>
2018-04-10 15:20:00 -0600

    created scaffold for <WeatherList />

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,7 +3,7 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
   case FETCH_WEATHER:
-    return [ action.payload.data, ...state ]; //this creates a new array instead of mutating an existing array.  Don't want to manipulate state.
+    return [ action.payload.data, ...state ]; // same as return state.concat([action.payload.data]) this creates a new array instead of mutating an existing array.  Don't want to manipulate state.
   }
   return state;
 }
