2b66bf8a5c6a35ff70544678291fee1c1c268387
Mark Brooks <Markabrooks@gmail.com>
2018-04-13 18:57:37 -0600

    added units and average bar to graphs

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import _ from 'lodash';
 
+function average(data) {
+  return _.round(_.sum(data)/data.length);
+}
 //this is just a functional component - we don't need to change state
 export default (props) => {
   return(
     <div>
       <Sparklines height={120} width={180} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{average(props.data)} {props.units}</div>
     </div>
   )
 }
